c5ba8f369e754ee4c1026511f7e5e72e96853d45
L200170014 <47962423+L200170014@users.noreply.github.com>
2019-11-26 13:57:22 +0700

    Add files via upload

---
+++ Modul 9/admin.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Selamat Datang Admin</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+    <br>
+    <center><marquee direction="up"><h1>Selamat Datang Admin</h1></marquee>
+    <p class="color">Silahkan logout dengan klik link <a href="logout2.php" class="link">Disini</a></p></center>
+</body>
+</html>
---
+++ Modul 9/member.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Selamat Datang Member</title>
+    <link rel="stylesheet" href="style1.css" type="text/css">
+</head>
+<body>
+    <br>
+    <center><marquee direction="up"><h1>Selamat Datang Member</h1></marquee>
+    <p class="warna">Silahkan logout dengan klik link <a href="logout2.php" class="link">Disini</a></p></center>
+</body>
+</html>
